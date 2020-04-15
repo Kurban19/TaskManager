c49588e662155f5fb8b4d339c0b2f7ea8e879b5d
Shkiper01 <baga5828391@gmail.com>
2020-04-15 13:17:03 +0300

    Set Github

---
+++ app/src/main/java/com/shkiper/taskmanager/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.shkiper.taskmanager
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.github.mikephil.charting.data.ChartData
+import com.github.mikephil.charting.data.PieData
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+    }
+}
